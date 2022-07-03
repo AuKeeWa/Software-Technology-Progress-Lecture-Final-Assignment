55bab44fa29ec979a57766756797617d0e52c3bb
AuKeeWa <aukeewa@outlook.com>
2022-07-03 16:31:16 +0800

    each computer needs how much power

--- Solution.java
+++ Solution.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+
 public class Solution {
     public long maxRunTime(int n, int[] batteries) {
+        Arrays.sort(batteries);
+        long sum = 0;
+        for(int i:batteries){
+            sum+=i;
+        }
+        for (int i = batteries.length-1; i >=0 ; i--) {
+            if(batteries[i] > sum/n){
 
+            }
+        }
         return 0;
     }
 }
